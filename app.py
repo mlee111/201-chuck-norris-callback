5257d4123677c571a64647ef20576a4406f70795
mlee111 <michelle_lee1@intuit.com>
2022-07-19 22:48:52 +0000

    new images

--- app.py
+++ app.py
@@ -5,24 +5,25 @@ import dash_html_components as html
 import os
 
 ###### Set up variables
-list_of_choices=['punch', 'body-slam', 'round-house kick to the face']
+list_of_choices=['Pick your meal', 'McDouble', 'McRib', 'McNuggets', 'McChicken']
 githublink = 'https://github.com/austinlasseter/chuck_norris_execution'
-image1='chucknorris.jpg'
-heading1='Chuck Norris execution method'
+image1='download.jpg'
+list_of_images=['download.jpg', 'double.jpeg', 'rib.jpeg', 'nuggies.jpeg', 'chicken.jpeg']
+heading1='McDonald\'s menu'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='Chuck'
+app.title='Mickey D\'s'
 
 ####### Layout of the app ########
 app.layout = html.Div([
     html.H2(heading1),
-    html.Img(src=app.get_asset_url(image1), style={'width': 'auto', 'height': '10%'}),
+    html.Img(id='image-output'),
     dcc.Dropdown(id='your-input-here',
-                options=[{'label': i, 'value': i} for i in list_of_choices],
-                value='punch',
+                options=[{'label': list_of_choices[i], 'value': list_of_images[i]} for i in range(0, 4)],
+                value='Pick your meal',
                 style={'width': '500px'}),
     html.Br(),
     html.Div(id='your-output-here', children=''),
@@ -36,7 +37,15 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('your-output-here', 'children'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return f'Chuck Norris will now execute you with a {whatever_you_chose}.'
+    if (whatever_you_chose != 'Pick your meal'):
+        return f'You have ordered a {whatever_you_chose}.'
+    else:
+        return 'Pick a menu item'
+
+@app.callback(dash.dependencies.Output('image-output', 'src'),
+              [dash.dependencies.Input('your-input-here', 'label')])
+def display_value(whatever_you_chose):
+    return whatever_you_chose
 
 
 ######### Run the app #########
